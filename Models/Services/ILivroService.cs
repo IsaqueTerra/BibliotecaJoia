9399740308cc3e6a5df44250307b33ddb1a8aaf2
IsaqueTerra <89212906+IsaqueTerra@users.noreply.github.com>
2021-12-31 15:23:41 -0300

    Initial commit

---
+++ Models/Services/ILivroService.cs
@@ -0,0 +1,11 @@
+﻿using BibliotecaJoia.Models.Dtos;
+using System.Collections.Generic;
+
+namespace BibliotecaJoia.Models.Services
+{
+    public interface ILivroService
+    {
+        List<LivroDto> Listar();
+
+    }
+}
